0648a1df284d42524da7e8cde818e7d9656b0ff3
Rakib <rh.rakibhasan365@gmail.com>
2025-02-17 16:06:32 +0600

    products api create

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
       
+      const productsCollection = client.db("babyShopDB").collection("products");
+
+      //   products endPoints
+      app.get("/products", async (req, res) => {
+          const result = await productsCollection.find().toArray();
+          res.send(result);
+      });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
